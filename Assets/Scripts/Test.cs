a9039cea15d25a1be50c297e873ceae96a921b9d
Zaur Kurbanismailov <kurbanismailov.z@azurgames.com>
2024-01-17 03:10:38 +0300

    Move files to package folder

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Codomaster.Extensions;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private void Start()
+    {
+        transform.SetPositionX(1f);
+    }
+}
